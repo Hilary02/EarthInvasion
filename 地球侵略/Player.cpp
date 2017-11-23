a3e28ca51d189c3892e68508e49770dd51a03cd4
hanoi <kandenchi4389@gmail.com>
2017-11-23 20:14:14 +0900

    まともな飛ぶええやん

--- 地球侵略/Player.cpp
+++ 地球侵略/Player.cpp
@@ -22,35 +22,37 @@ Player::~Player() {
 
 //計算処理
 void Player::Update() {
-	if (keyM.GetKeyFrame(KEY_INPUT_LEFT) >= 1 && !attackFlag) {
+	if (keyM.GetKeyFrame(KEY_INPUT_LEFT) >= 1 && !isAttack) {
 		right = false;
-		if (MapHitCheck(-MOVE, 0)) {
+		xyCheck = 'x';
+		if (MapHitCheck(x - MOVE, y, xyCheck) && MapHitCheck(x - MOVE, y + 63, xyCheck)) {
 			x -= MOVE;
 		}
 		else {
 			x -= cMove;
 		}
 	}
-	if (keyM.GetKeyFrame(KEY_INPUT_RIGHT) >= 1 && !attackFlag) {
+	if (keyM.GetKeyFrame(KEY_INPUT_RIGHT) >= 1 && !isAttack) {
 		right = true;
-		if (MapHitCheck(MOVE, 0)) {
+		xyCheck = 'x';
+		if (MapHitCheck(x + MOVE + 63, y, xyCheck) && MapHitCheck(x + MOVE + 63, y + 63, xyCheck)) {
 			x += MOVE;
 		}
 		else {
 			x += cMove;
 		}
 	}
-	if (keyM.GetKeyFrame(KEY_INPUT_DOWN) >= 1 && !isJumping && !attackFlag)
-		liquidFlag = true;
+	if (keyM.GetKeyFrame(KEY_INPUT_DOWN) >= 1 && !isJumping && !isAttack)
+		isLiquid = true;
 	else if (keyM.GetKeyFrame(KEY_INPUT_DOWN) == 0)
-		liquidFlag = false;
+		isLiquid = false;
 
-	if (keyM.GetKeyFrame(KEY_INPUT_UP) == 1 && !liquidFlag && !attackFlag) {
+	if (keyM.GetKeyFrame(KEY_INPUT_UP) == 1 && !isLiquid && !isAttack) {
 		isJumping = true;
-		jumpPower = -20;
+		jumpPower = -5;
 	}
-	if (keyM.GetKeyFrame(KEY_INPUT_A) == 1 && !attackFlag) {
-		attackFlag = true;
+	if (keyM.GetKeyFrame(KEY_INPUT_A) == 1 && !isAttack) {
+		isAttack = true;
 		//rect();
 		drawCount = 0;
 	}
@@ -61,17 +63,25 @@ void Player::Update() {
 
 	}
 
-	if (MapHitCheck(0, 1)) {
+	xyCheck = 'y';
+	if (MapHitCheck(x, y + 64, xyCheck) && MapHitCheck(x + 63, y + 64, xyCheck)) {
 		isJumping = true;
 	}
 	else {
 		isJumping = false;
+		jumpPower = 0;
 	}
 
 	if (isJumping) {
-		if (MapHitCheck(0, jumpPower)) {
+		xyCheck = 'y';
+		if (MapHitCheck(x, y + jumpPower + 63, xyCheck) && MapHitCheck(x + 63, y + jumpPower + 63, xyCheck)) {
 			y += jumpPower;
-			jumpPower += 1;
+			clock++;
+			if (clock >= 5) {
+				if(jumpPower <= 4)
+					jumpPower += 1;
+				clock = 0;
+			}
 		}
 		else {
 			y += cMove;
@@ -79,40 +89,53 @@ void Player::Update() {
 
 	}
 	if (hp <= 0) {
-		deadFlag = true;
+		isDead = true;
 		//GameOver();
 	}
 
 }
 
-bool Player::MapHitCheck(int moveX, int moveY)
+bool Player::MapHitCheck(int movedX, int movedY, bool check)
 {
-	DrawFormatString(100, 120, 0xFFFFFF, "Seeing:%d,%d",  ((int)this->x + moveX)/32,((int)this->y + moveY) / 32 );
+	DrawFormatString(100, 120, 0xFFFFFF, "Seeing:%d,%d", movedX / 32, movedY / 32);
 	//DrawFormatString(100, 120, 0x00, "%d", vmap[((int)this->y + moveY) / 32][((int)this->x + moveX) / 32]);
-	switch (vmap[((int)this->y + moveY) / 32+1 ][((int)this->x + moveX) / 32+1] ) {
+	switch (vmap[movedY / 32][movedX / 32]) {
 	case 0:
 		return true;
 		break;
 	case 1:
 		DrawFormatString(200, 140, 0xFFFFFF, "見えない壁だ！");
-		if (moveX > 0)
-			cMove = moveX - ((int)this->x + moveX) % 32;
-		else if (moveX < 0)
-			cMove = (int)this->x % 32;
+		if (check == 'x') {
+			if (movedX - x > 0)
+				cMove = (movedX - x) - (movedX % 32 + 1);
+			else if (movedX - x < 0)
+				cMove = x % 32;
+		}
+		if (check == 'y') {
+			if (movedY - (y + 63) > 0) {
+				cMove = (movedY - (y + 63)) - (movedY % 32 + 1);
+			}
+			else if (movedY - y < 0) {
+				cMove = y % 32;
+			}
+		}
 		return false;
 		break;
 	case 2:
 		DrawFormatString(200, 140, 0xFFFFFF, "壁だ！");
-		if (moveX > 0)
-			cMove = moveX - ((int)this->x + moveX) % 32;
-		else if (moveX < 0)
-			cMove = (int)this->x % 32;
-
-		if (moveY > 0)
-			cMove = moveY - ((int)this->y + moveY) % 32;
-		else if (moveY < 0) {
-			cMove = (int)this->y % 32;
-			jumpPower = 0;
+		if (check == 'x') {
+			if (movedX - x > 0)
+				cMove = movedX - x - (movedX % 32 + 1);
+			else if (movedX - x < 0)
+				cMove = x % 32;
+		}
+		if (check == 'y') {
+			if (movedY - (y + 63) > 0) {
+				cMove = (movedY - (y + 63)) - (movedY % 32 + 1);
+			}
+			else if (movedY - y < 0) {
+				cMove = y % 32;
+			}
 		}
 		return false;
 		break;
@@ -120,7 +143,9 @@ bool Player::MapHitCheck(int moveX, int moveY)
 		return true;
 		break;
 	case 9:
-		cMove = moveY - ((int)this->y + moveY) % 32;
+		if (movedY - (y + 63) > 0) {
+			cMove = (movedY - (y + 63)) - (movedY % 32 + 1);
+		}
 		return false;
 		break;
 	default:
@@ -147,14 +172,14 @@ void Player::Draw(int drawX, int drawY) {
 				else if (jumpPower < -1)
 					DrawGraph(tempX, tempY, jump[2], TRUE);
 			}
-			else if (liquidFlag) {
+			else if (isLiquid) {
 				drawCount = keyM.GetKeyFrame(KEY_INPUT_DOWN) / 15 % 4;
 				DrawGraph(tempX, tempY, wait[drawCount], TRUE);//仮
 			}
-			else if (attackFlag) {
+			else if (isAttack) {
 				DrawGraph(tempX, tempY, attack[drawCount / 8 % 8], true);
 				drawCount++;
-				if (drawCount >= 64) attackFlag = false;
+				if (drawCount >= 64) isAttack = false;
 			}
 			else if (keyM.GetKeyFrame(KEY_INPUT_RIGHT) >= 1) {
 				drawCount = keyM.GetKeyFrame(KEY_INPUT_RIGHT) / 15 % 4;
@@ -242,14 +267,14 @@ void Player::Draw(int drawX, int drawY) {
 				else if (jumpPower < -1)
 					DrawTurnGraph(tempX, tempY, jump[2], TRUE);
 			}
-			else if (liquidFlag) {
+			else if (isLiquid) {
 				drawCount = keyM.GetKeyFrame(KEY_INPUT_DOWN) / 15 % 4;
 				DrawTurnGraph(tempX, tempY, wait[drawCount], TRUE);//仮
 			}
-			else if (attackFlag) {
+			else if (isAttack) {
 				DrawTurnGraph(tempX, tempY, attack[drawCount / 8 % 8], true);
 				drawCount++;
-				if (drawCount >= 64) attackFlag = false;
+				if (drawCount >= 64) isAttack = false;
 			}
 			else if (keyM.GetKeyFrame(KEY_INPUT_LEFT) >= 1) {
 				drawCount = keyM.GetKeyFrame(KEY_INPUT_LEFT) / 15 % 4;
@@ -322,10 +347,10 @@ void Player::Draw(int drawX, int drawY) {
 	}
 	//DrawFormatString(100, 100, 0xFFFFFF, "%d,%d", tempX, tempY);
 	DrawFormatString(100, 80, 0xFFFFFF, "Player:%d,%d", (int)x, (int)y);
-	DrawFormatString(100, 100, 0xFFFFFF, "MapChip:%d,%d", (int)(x/32), (int)(y/32));
-	if(isJumping) DrawFormatString(300, 80, 0xFFFFFF, "ジャンプ中");
-	if (attackFlag) DrawFormatString(300, 100, 0xFFFFFF, "アタック中");
-	if (deadFlag) DrawFormatString(300, 120, 0xFFFFFF, "死んだ");
+	DrawFormatString(100, 100, 0xFFFFFF, "MapChip:%d,%d", (int)(x / 32), (int)(y / 32));
+	if (isJumping) DrawFormatString(300, 80, 0xFFFFFF, "ジャンプ中");
+	if (isAttack) DrawFormatString(300, 100, 0xFFFFFF, "アタック中");
+	if (isDead) DrawFormatString(300, 120, 0xFFFFFF, "死んだ");
 
 }
 int Player::getX()
--- 地球侵略/Player.h
+++ 地球侵略/Player.h
@@ -14,18 +14,20 @@ public:
 	int getY();
 private:
 	std::vector<std::vector <int>> vmap;
+	int clock = 0;
 	const int MOVE = 2;
 	int drawCount = 0;
 	int cMove = 0;
 	void LoadImg();
-	bool MapHitCheck(int , int);
+	bool MapHitCheck(int , int , bool);
 	int jumpPower = 0;
 	double speed = 10.0;
 	bool right = true;
 	bool isJumping = false;
-	bool liquidFlag = false;
-	bool attackFlag = false;
-	bool deadFlag = false;
+	bool isLiquid = false;
+	bool isAttack = false;
+	bool isDead = false;
+	char xyCheck = 'N';
 	char plState = 'N';
 	int wait[5];
 	int move[5];
