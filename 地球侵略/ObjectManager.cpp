61236d683551f54a8d50094595173eff690afdb5
LAPTOP-6MT1B9JB\m011715141 <m011715141@edu.teu.ac.jp>
2017-12-07 18:19:10 +0900

    ObjectManager追記

--- 地球侵略/ObjectManager.cpp
+++ 地球侵略/ObjectManager.cpp
@@ -12,15 +12,29 @@ ObjectManager::ObjectManager(std::vector<std::vector <int>> vmap) {
 				path = img[vmap[i][j]];
 				switch (vmap[i][j])
 				{
-
-				/*case 3:
+				case 3:
 					obje = GroundFloor(x, y, img[3]);
-					break;*/
-				/*case 10:
-					obje = Enemy(x, y, img[4]);
-					break;*/
+					break;
+				case 10:
+					obje = Enemy(x, y, img[10]);
+					break;
+				case 11:
+					obje = Enemy(x, y, img[11]);
+					break;
+				case 12:
+					obje = Enemy(x, y, img[12]);
+					break;
+				case 13:
+					obje = Enemy(x, y, img[13]);
+					break;
+				case 14:
+					obje = Enemy(x, y, img[14]);
+					break;
 				case 20:
 					obje = Item(x, y, img[20]);
+					break;
+				case 21:
+					obje = Item(x, y, img[21]);
 					break;
 
 				default:
@@ -40,13 +54,12 @@ ObjectManager::~ObjectManager() {
 void ObjectManager::Loadimg() {
 	img[3] = LoadGraph("data/img/groundFloor.png");  //3は動く床
 	img[10] = LoadGraph("data/img/enemy1Wait.png");   //10は一般兵
-	img[11] = LoadGraph("data/img/");   //11はベテラン兵
-	img[12] = LoadGraph("data/img/");   //12は大佐
-	img[13] = LoadGraph("data/img/");   //13は棒人間
+	img[11] = LoadGraph("data/img/");   //11は中将
+	img[12] = LoadGraph("data/img/");   //12はベテラン兵
+	img[13] = LoadGraph("data/img/");   //13は毒人間
 	img[14] = LoadGraph("data/img/");   //14は魔法使い
 	img[20] = LoadGraph("data/img/healPot.png");    //20は回復薬
-	img[21] = LoadGraph("data/img/curePod.png"); //21は回復薬
-	
+	img[21] = LoadGraph("data/img/curePod.png"); //21は毒消し
 }
 
 void ObjectManager::Update()
