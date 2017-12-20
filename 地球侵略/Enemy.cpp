3739a2872972e86c34a9f3492ccc8289ddca3a5d
Jun Yamahira <c0116278ba@edu.teu.ac.jp>
2017-12-20 17:56:05 +0900

    攻撃モーション実装！

--- 地球侵略/Enemy.cpp
+++ 地球侵略/Enemy.cpp
@@ -16,7 +16,11 @@ Enemy::Enemy(int x, int y, int img, int id, ICollisionManager* IcolMgr){
 	this->imgHandle = img;
 	this->enemyID = id;
 	LoadDivGraph("data/img/enemy1Walk.png", 8, 4, 2, 64, 64, walk);
+	LoadDivGraph("data/img/enemy1Atack.png", 4, 4, 1, 64, 64, atackHundle);
 	collision = new Collision(colXOffset, colYOffset, colXSize, colYSize);
+//	AttackBox = new Collision(atackRen, colYOffset, 320, colYSize);
+	AttackBox = new Collision(32, colYOffset, -160, colYSize);
+
 	this->IcolMgr = IcolMgr;
 }
 
@@ -31,13 +35,16 @@ Enemy::Enemy(int x, int y, int img, int id, ICollisionManager* IcolMgr){
 
 void Enemy::Update(const Collision & playerCol)
 {
-	MoveCommon();
 	collision->updatePos(x, y);
+	AttackBox->updatePos(x, y);
 	collisionCheck(playerCol);
 }
 
 void Enemy::Draw(int drawX, int drawY)
 {
+	DrawBox(AttackBox->hitRange.xPos + AttackBox->hitRange.xOffset -drawX, AttackBox->hitRange.yPos + AttackBox->hitRange.yOffset - drawY, AttackBox->hitRange.xPos + AttackBox->hitRange.xOffset - drawX+AttackBox->hitRange.xSize, AttackBox->hitRange.yPos + AttackBox->hitRange.yOffset - drawY+AttackBox->hitRange.ySize, 0x00FF00, false);
+
+
 	DrawFormatString(0, 0, 0xFFFFFF, "Enemy:%d,%d     Draw:%d,%d", x, y, x - drawX, y - drawY);
 	isRight = IsRangeCheck();
 	if (isRight)
@@ -52,35 +59,54 @@ void Enemy::Draw(int drawX, int drawY)
 
 void Enemy::collisionCheck(const Collision & target) {
 	int isCol = collision->doCollisonCheck((target.hitRange));
+	int attackR = AttackBox->doCollisonCheck((target.hitRange));
 	if (isCol) {
+		movedis = 0;
 		DrawBox(10, 20, 100, 200, 0xFF0000, true);
 		IcolMgr->requestAction(Action::DmgPlayer);
 	}
+	else if(attackR){
+		DrawBox(10,20,100,200, 0x0000ff,true);
+		AtackCommon();
+	}
 	else {
 		DrawBox(10, 20, 100, 200, 0xFF0000, false);
+		MoveCommon();
 	}
 }
 
 void Enemy::MoveCommon()
 {
+	movedis = 1;
 	if (isRight)
 	{
-		x += MOVE;
+		x += movedis;
 	}
 	else
 	{
-		x -= MOVE;
+		x -= movedis;
 	}
 	imgHandle = walk[(drawcount / 8) % 8];
+	drawcount++;
+	
+	//countƒŠƒZƒbƒgŒã‰ñ‚µ
+	//if (drawcount == 72) drawcount = 0;
+}
+
+void Enemy::AtackCommon()
+{
+	movedis = 0;
+	imgHandle = atackHundle[(drawcount / 12) % 4];
 	drawcount++;
 }
 
 bool Enemy::IsRangeCheck()
 {
-	dis += MOVE;
+	dis += movedis;
 	if (moveRange < dis)
 	{
 		dis = 0;
+		AttackBox->flip();
 		return !isRight;
 	}
 	else
--- 地球侵略/Enemy.h
+++ 地球侵略/Enemy.h
@@ -16,17 +16,21 @@ public:
 	void Draw(int drawX, int drawY) override;
 	void collisionCheck(const Collision& target);
 	void MoveCommon();
+	void AtackCommon();
 	bool IsRangeCheck();
 	int enemyID;
 
 private:
 	int drawcount = 0;
 	int walk[8];
+	int atackHundle[4];
+	int atackRen = -128;
 	const int moveRange  = 300;
 	int dis = 150;
 	bool isRight = false;
-	const int MOVE = 1;
+	int movedis = 1;
 
 	Collision* collision;
+	Collision* AttackBox;
 };
 
