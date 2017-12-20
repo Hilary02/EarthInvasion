151e542901972bbee87139135fd0f055df36d0fe
Hilary <c011605154@edu.teu.ac.jp>
2017-12-20 19:09:31 +0900

    GameOverする Singleton準備

--- 地球侵略/MovingFloor.cpp
+++ 地球侵略/MovingFloor.cpp
@@ -2,8 +2,7 @@
 
 
 
-MovingFloor::MovingFloor()
-{
+MovingFloor::MovingFloor(){
 }
 
 
@@ -11,8 +10,17 @@ MovingFloor::~MovingFloor()
 {
 }
 
-void MovingFloor::Update(){
+void MovingFloor::Update(const Collision & playerCol){
+	Frame++;
+	if (Frame % 3 == 0) {
+		y += moveSpeed;
+		if (moveMax <= y) moveSpeed = -1;
+		if (moveMin >= y)moveSpeed = 1;
+
+	}
 }
 
-void MovingFloor::Draw(){
+void MovingFloor::Draw(int drawX, int drawY){
+	//DrawGraph(x-drawX,y-drawY,)
+	DrawBox(x - drawX, y - drawY,64,64, 0x00FFFF, true);
 }
--- 地球侵略/MovingFloor.h
+++ 地球侵略/MovingFloor.h
@@ -4,11 +4,15 @@ class MovingFloor :
 	public Object
 {
 public:
-	MovingFloor();
+	MovingFloor(int x, int y, int handle, ICollisionManager* IcolMgr);
 	~MovingFloor();
-	void Update();
-	void Draw();
+	void Update(const Collision & playerCol) override;
+	void Draw(int drawX, int drawY) override;
+	Collision collision;
 private:
-
+	int moveSpeed = 1;
+	int moveMax = 20;
+	int moveMin = -20;
+	int Frame = 0; //‚¢‚Â‚©‚­‚Á‚Â‚¯‚é
 };
 
