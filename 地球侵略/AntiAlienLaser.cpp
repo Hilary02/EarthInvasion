621450542d7bab70a52cec9baa8398fa8a3c647e
hanoi <kandenchi4389@gmail.com>
2018-04-26 20:40:03 +0900

    からっぽ

--- 地球侵略/AntiAlienLaser.cpp
+++ 地球侵略/AntiAlienLaser.cpp
@@ -1,5 +1,5 @@
 #include "AntiAlienLaser.h"
-
+#include "DxLib.h"
 
 
 AntiAlienLaser::AntiAlienLaser()
@@ -19,4 +19,36 @@ AntiAlienLaser::AntiAlienLaser(int x, int y, int hundle, ObjectID id)
 	this->id = id;
 
 	collision = new Collision(x, y, 128, 256);
+
+	
+}
+
+void AntiAlienLaser::update(const Collision) {
+	timer++;
+}
+
+void AntiAlienLaser::Draw(int drawX, int drawY) {
+	int tempX = x - drawX;
+	int tempY = y - drawY;
+
+	switch (id) {
+	case 1:
+		if (timer) {
+
+		}
+		break;
+	case 2:
+		if (timer) {
+
+		}
+		break;
+
+	}
+}
+
+void loadImg() {
+	LoadDivGraph("data/img/LaserA_Wait.png", 1, 1, 1, 32, 64, laserImg);
+	LoadDivGraph("data/img/LaserA_Shoot.png", 1, 1, 1, 64, 32, laserImg[1]);
+	LoadDivGraph("data/img/LaserB_Wait.png", 1, 1, 1, 32, 64, laserImg[10]);
+	LoadDivGraph("data/img/LaserB_shoot.png", 1, 1, 1, 32, 64, laserImg[11]);
 }
--- 地球侵略/AntiAlienLaser.h
+++ 地球侵略/AntiAlienLaser.h
@@ -7,7 +7,11 @@ public:
 	AntiAlienLaser();
 	~AntiAlienLaser();
 	AntiAlienLaser(int, int, int, ObjectID id);
-		
+	void AntiAlienLaser::update(const Collision);
+	void AntiAlienLaser::Draw(int, int);
 
+private:
+	int timer = 0;
+	int laserImg[20];
+	void loadImg();
 };
-
