3b0f10574d35e74f75d0c1f0332b7e0e5a56803e
LAPTOP-6MT1B9JB\m011715141 <m011715141@edu.teu.ac.jp>
2018-05-31 20:47:45 +0900

    biribiri修正

--- 地球侵略/Player.cpp
+++ 地球侵略/Player.cpp
@@ -207,6 +207,45 @@ int Player::update() {
 	}
 
 	//地形オブジェクトとの当たり判定をとり，位置の修正
+	for (auto t : IobjMgr->getObjectList()) {
+		if (collision->doCollisonCheck(t->collision->hitRange)) {
+			switch (t->getId()) {
+			case ObjectID::spark: //扉
+			{
+				int leftTX = t->collision->hitRange.xPos + t->collision->hitRange.xOffset;
+				int leftPX = collision->hitRange.xPos + collision->hitRange.xOffset;
+				int topTY = t->collision->hitRange.yPos + t->collision->hitRange.yOffset;
+				int topPY = collision->hitRange.yPos + collision->hitRange.yOffset;
+
+
+				if (topPY > topTY+20) {
+					y = topTY + t->collision->hitRange.ySize - collision->hitRange.yOffset;
+					jumpPower = 0;
+				}
+				else if (leftPX < leftTX) {
+					x = leftTX - collision->hitRange.xSize - collision->hitRange.xOffset;
+				}
+				else if (leftPX > leftTX) {
+					x = leftTX + t->collision->hitRange.xSize - collision->hitRange.xOffset;
+				}
+
+
+
+
+
+				isAttack = false;
+
+				break;
+			}
+			default:
+				break;
+			}
+		}
+	}
+
+
+
+	//地形オブジェクトとの当たり判定をとり，位置の修正
 	for (auto t : IobjMgr->getTerrainList()) {
 		if (collision->doCollisonCheck(t->collision->hitRange)) {
 			switch (t->getId()) {
@@ -239,7 +278,7 @@ int Player::update() {
 				}
 			}
 			break;
-
+			
 			default:
 				break;
 			}
